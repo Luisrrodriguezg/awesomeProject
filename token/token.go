4c36f83977ff92f9a5a4fdc191904ec4b54be705
luisrro <luis.alejo.rg@gmail.com>
2025-04-24 10:06:21 -0500

    Initial commit

---
+++ token/token.go
@@ -0,0 +1,64 @@
+package token
+
+type TokenType string
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const ( //All of our tokens
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// IDENT Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+
+	// ASSIGN Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
+
+	// COMMA Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// FUNCTION Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+	EQ       = "=="
+	NOT_EQ   = "!="
+)
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
